d6ffeeb6ba6a1f308e76555bd463992cd2f8d141
AdrianJay Telan <atelan@odin.cs.csubak.edu>
2021-05-07 14:27:18 -0700

    adrian: implemented constMult functionality

--- matrix.rb
+++ matrix.rb
@@ -23,6 +23,25 @@ end
 ###IDENTITY MATRIX HERE
 
 ###SCALAR HERE 
+def constMult(matrix, const)
+    row = matrix[0].length
+    col = matrix.length
+    newMatrix = Array.new(col){Array.new(row)}
+
+    i = 0
+    j = 0
+
+    matrix.each do |mRow|
+        mRow.each do |mCol|
+            newMatrix[i][j] = mCol * const
+            j = j + 1
+        end
+        i = i + 1
+        j = 0
+    end
+    return newMatrix
+
+end
 
 def Determinate(matrix)
 
@@ -136,25 +155,74 @@ while true
     case choice
         when 0
             break
+        
         when 1
             matrixA = LoadA()
             print matrixA
+        
         when 2
             matrixB = LoadB()
             print matrixB
+        
+        when 5
+            puts "Please enter a value for 'n': \n"
+            inp = Integer(gets) rescue false
+            while inp == false
+                puts "Please enter an INTEGER for 'n': \n"
+                inp = Integer(gets) rescue false
+            end
+            matrixC = constMult(matrixA, inp)
+            print matrixC
+            puts "\n"
+            
+            puts "Would you like to store the result of A? (Y/N)\n"
+            inp = gets.chomp
+            while inp != "Y" && inp != "N"
+                puts "Please input 'Y' for yes, 'N' for no.\n"
+                inp = gets.chomp
+            end
+            if inp == "Y"
+                matrixA = matrixC
+            end
+        when 6
+            puts "Please enter a value for 'n': \n"
+            inp = Integer(gets) rescue false
+            while inp == false
+                puts "Please enter an INTEGER for 'n': \n"
+                inp = Integer(gets) rescue false
+            end
+            matrixC = constMult(matrixB, inp)
+            print matrixC
+            puts "\n"
+            
+            puts "Would you like to store the result of B? (Y/N)\n"
+            inp = gets.chomp
+            while inp != "Y" && inp != "N"
+                puts "Please input 'Y' for yes, 'N' for no.\n"
+                inp = gets.chomp
+            end
+            if inp == "Y"
+                matrixB = matrixC
+            end
+        
         when 13
             matrixC = addMatrix(matrixA, matrixB) 
             print matrixC
+        
         when 14
             matrixC = subMatrix(matrixB, matrixA)
             print matrixC
+        
         when 15
             matrixC = subMatrix(matrixA, matrixB)
             print matrixC
+        
         when 21
             print matrixA
+        
         when 22
             print matrixB
+        
         else
             puts "INVALID INPUT"
     end
